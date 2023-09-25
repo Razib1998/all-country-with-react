9c887feaa28dd96580ac6c1c212f91e2a439ff76
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2023-09-25 16:24:46 +0200

    final commit

--- react-simple-project/src/components/Countries/Countries.jsx
+++ react-simple-project/src/components/Countries/Countries.jsx
@@ -1,22 +1,63 @@
 import { useEffect, useState } from "react";
 import Country from "../Country/Country";
+import './countries.css'
 
 
 const Countries = () => {
 
     const [countries, setCountries] = useState([]);
+    // To show the visited country in visited countries list.
+    const [visitedCountries, setVisitedCountries] = useState([]);
+    // to show visited country flag
+
+    const [visitedCountriesFlag,setVisitedCountriesFlags] = useState([]);
+
     useEffect(() => {
         fetch("https://restcountries.com/v3.1/all")
           .then(res => res.json())
           .then(data => setCountries(data));
     },[])
+
+  const handleVisitedCountry = country =>{
+    const newVisitedCountries = [...visitedCountries, country];
+    setVisitedCountries(newVisitedCountries);
+  }
+  const handleVisitedCountriesFlag = flag => {
+   const newVisitedCountriesFlags = [...visitedCountriesFlag, flag];
+   setVisitedCountriesFlags(newVisitedCountriesFlags);
+  }
     return (
+      <div>
+        <h3>Countries: {countries.length}</h3>
         <div>
-          <h3>Countries: {countries.length}</h3> 
-          {
-            countries.map(country => <Country key={country.cca3} country={country}></Country>)
-          } 
+          <h3>Visited countries: {visitedCountries.length}</h3>
+
+          {/* For visited countries list */}
+          <ul>
+            {visitedCountries.map((country) => (
+              <li key={country.cca3}>{country.name.common}</li>
+            ))}
+          </ul>
         </div>
+        <div className="flag-container">
+         {
+          visitedCountriesFlag.map((flag, idx )=> <img key={idx} src = {flag}></img>)
+         }
+        </div>
+
+        {/* Display country */}
+
+        <div className="country-container">
+          {countries.map((country) => (
+            <Country
+              key={country.cca3}
+              handleVisitedCountry={handleVisitedCountry}
+              handleVisitedCountriesFlag={handleVisitedCountriesFlag}
+              country={country}
+            ></Country>
+          ))}
+        </div>
+      </div>
     );
 };
 
--- react-simple-project/src/components/Country/Country.jsx
+++ react-simple-project/src/components/Country/Country.jsx
@@ -1,17 +1,35 @@
+import { useState } from 'react';
 import './Country.css';
 
-const Country = ({country}) => {
+const Country = ({ country, handleVisitedCountry,handleVisitedCountriesFlag }) => {
   console.log(country);
-  const {name, continents, flags} = country;
-    return (
-        <div className='country'>
-          <h3>Name: {name?.common}</h3>  
-          <h3>Continents: {continents}</h3>
-          <img src= {flags.png} alt="" />  
-        
-      
-        </div>
-    );
+
+  const [visited, setVisited] = useState(false);
+  const handleVisited = () => {
+    setVisited(!visited);
+  }
+
+  const { name, continents, flags, population, area, cca3 } = country;
+  return (
+    <div className="country">
+      <h3>Name: {name?.common}</h3>
+      <h3>Continents: {continents}</h3>
+      <p>Population: {population}</p>
+      <p>Area: {area}</p>
+      <p>
+        <small>Code: {cca3}</small>
+      </p>
+      <img src={flags.png} alt="" />
+      <button onClick={() => handleVisitedCountry(country)}>
+        Mark As Visited
+      </button>{" "}
+      <br /> <br />
+      <br />
+      <br />
+      <button onClick={() => handleVisitedCountriesFlag(country.flags.png)}>Add Flag</button>
+      {visited ? "I have Already visit this country" : "I want to visit"}
+    </div>
+  );
 };
 
 export default Country;
